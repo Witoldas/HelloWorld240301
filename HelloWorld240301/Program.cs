aacb615762ad08dc64dafdb23a70b85f87b14e8e
Witoldas <70074120+Witoldas@users.noreply.github.com>
2024-02-28 13:57:41 +0100

    Add project files.

---
+++ HelloWorld240301/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld240301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
